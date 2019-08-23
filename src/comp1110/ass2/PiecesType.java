f68ce73c3c6b23eb1a84de799f63f4d21ac8d548
Yixi Rao <u6826541@anu.edu.au>
2019-08-23 12:44:22 +1000

    stage c Yixi Rao edited on Game and PiecesType

--- src/comp1110/ass2/PiecesType.java
+++ src/comp1110/ass2/PiecesType.java
@@ -7,10 +7,8 @@ public enum PiecesType {
 
     public Location[] createPiece(int x,int y, Orientation orientation){
         int[] pieceOffsets = pieceMapOffset[this.ordinal()][Integer.parseInt(orientation.toChar()+"")];
-        System.out.println(Integer.parseInt(orientation.toChar()+""));//$1$
         Location[] locations = new Location[pieceOffsets.length/2];
         for (int i = 0;i < pieceOffsets.length / 2;i++){
-            System.out.println(pieceOffsets[2*i]+","+pieceOffsets[2*i + 1]);
             locations[i] = new Location(x + pieceOffsets[2*i],y + pieceOffsets[2*i + 1]);
         }
         return locations;
@@ -23,15 +21,15 @@ public enum PiecesType {
             {{2,0,0,1,1,1,2,1,3,1}, {0,0,0,1,0,2,1,2,0,3}, {0,0,1,0,2,0,3,0,1,1}, {1,0,0,1,1,1,1,2,1,3}},
             {{0,0,1,0,2,0,2,1}, {1,0,1,1,0,2,1,2}, {0,0,0,1,1,1,2,1}, {0,0,1,0,0,1,0,2}},
             {{0,0,1,0,2,0,0,1,1,1}, {0,0,1,0,0,1,1,1,1,2}, {1,0,2,0,0,1,1,1,2,1}, {0,0,0,1,1,1,0,2,1,2}},
-            {{0,0,1,0,2,0}, {0,0,0,1,0,2}},
-            {{0,0,1,0,1,1,2,1}, {1,0,0,1,1,1,0,2}},
+            {{0,0,1,0,2,0}, {0,0,0,1,0,2},{0,0,1,0,2,0}, {0,0,0,1,0,2}},
+            {{0,0,1,0,1,1,2,1}, {1,0,0,1,1,1,0,2},{0,0,1,0,1,1,2,1}, {1,0,0,1,1,1,0,2}},
             {{0,0,1,0,2,0,0,1,0,2}, {0,0,1,0,2,0,2,1,2,2}, {2,0,2,1,0,2,1,2,2,2}, {0,0,0,1,0,2,1,2,2,2}},
             {{0,0,1,0,1,1}, {1,0,0,1,1,1}, {0,0,0,1,1,1}, {0,0,1,0,0,1}},
-            {{0,0,1,0,2,0,3,0,0,1}, {0,0,1,0,1,1,1,2,1,3}, {0,3,0,1,1,1,2,1,3,1}, {0,0,0,1,0,2,0,3,1,3}}
+            {{0,0,1,0,2,0,3,0,0,1}, {0,0,1,0,1,1,1,2,1,3}, {3,0,0,1,1,1,2,1,3,1}, {0,0,0,1,0,2,0,3,1,3}}
     };
 
     public static void main(String[] args) {
-        Location[] l = A.createPiece(0,0,SOUTH);
+        Location[] l = J.createPiece(3,3,SOUTH);
         for (Location i:l) {
             System.out.println(i.getX()+"|"+i.getY());
         }
