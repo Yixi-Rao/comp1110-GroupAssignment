31dbde166fa9b9dc2468cde00ba1f3df51851de5
Wenxuan Li <u6825691@anu.edu.au>
2019-09-17 11:28:23 +1000

    d-originality

--- src/gittest/Main.java
+++ src/gittest/Main.java
@@ -2,11 +2,11 @@ package gittest;
 
 public class Main {
     public void main(){
-        A a = new A();
-
-        C c = new C();
-        A a = new A();
-
-        B b = new B();
+//        A a = new A();
+//
+//        C c = new C();
+//        A a = new A();
+//
+//        B b = new B();
     }
 }
